a77f470b43b21497be64d378807155ac1496ca7f
arunkumarsubramaniyan <107799036+arunkumarsubramaniyan@users.noreply.github.com>
2023-12-04 16:40:09 +0530

    Delete main.ts

--- main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import './assets/tailwind/tailwind.css'
-import router from "./router/index.ts";
-
-createApp(App).use(router).mount('#app')
